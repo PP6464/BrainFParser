1eb74c4cb8256037c48d5dcc1402f98dcf331c03
PP6464 <panthhp@outlook.com>
2023-06-13 12:28:11 +0100

    format comments

--- src/main/kotlin/parser/FormatCode.kt
+++ src/main/kotlin/parser/FormatCode.kt
@@ -3,10 +3,10 @@ package parser
 val validChars = listOf('+', '-', '>', '<', '.', ',', '[', ']')
 
 /*
-* params:
-* code: the code to format
-* returns:
-* the code without any comments or whitespaces and on one line
+    * params:
+    * code: the code to format
+    * returns:
+    * the code without any comments or whitespaces and on one line
 */
 fun formatCodeForParsing(code: String): String {
     return code.filter { validChars.contains(it) }
