9204778cb7a0c1cb4158c8af093664436a2f484b
Panth Patel <panthhp@outlook.com>
2023-06-13 20:55:19 +0100

    validate for error

--- src/main/kotlin/parser/FormatCode.kt
+++ src/main/kotlin/parser/FormatCode.kt
@@ -9,5 +9,11 @@ val validChars = listOf('+', '-', '>', '<', '.', ',', '[', ']')
     * the code without any comments or whitespaces and on one line
 */
 fun formatAndValidateCodeForParsing(code: String): String {
+    var filteredForValidation = code.filter { it == '[' || it == ']' }
+    while (filteredForValidation.isNotBlank()) {
+        val filteredForValidationCopy = filteredForValidation
+        filteredForValidation = filteredForValidation.replace("[]", "")
+        if (filteredForValidationCopy == filteredForValidation) throw Error("Ensure that your brackets are balanced and are in the correct order")
+    }
     return code.filter { validChars.contains(it) }
 }
