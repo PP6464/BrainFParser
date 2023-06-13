95ab5f0226e1ce5a7ebf44258763eb4a3c65d9bf
PP6464 <panthhp@outlook.com>
2023-06-13 11:34:24 +0100

    format and validate code for parser to parse

---
+++ src/main/kotlin/parser/FormatCodeForParsing.kt
@@ -0,0 +1,13 @@
+package parser
+
+val validChars = listOf('+', '-', '>', '<', '.', ',', '[', ']')
+
+/*
+* params:
+* code: the code to format
+* returns:
+* the code without any comments or whitespaces and on one line
+*/
+fun formatCodeForParsing(code: String): String {
+    return code.filter { validChars.contains(it) }
+}
